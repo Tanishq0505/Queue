2b2d4bc995b03aa16255b8f4d2b8a67d2bc0b924
Tanishq0505 <126601008+Tanishq0505@users.noreply.github.com>
2024-04-14 17:25:21 +0530

    Create 7_First_negative_integer in_every_window_of_size_k.cpp

---
+++ 7_First_negative_integer in_every_window_of_size_k.cpp
@@ -0,0 +1,50 @@
+// First negative integer in every window of size k (GFG) 
+// https://youtu.be/_gJ3to4RyeQ?list=PLDzeHZWIZsTryvtXdMr6rPh4IDexB5NIA 
+// lec 61 see from 8 min 29 sec Q2 
+
+vector<long long> printFirstNegativeInteger(long long int A[],
+                                             long long int N, long long int K) {
+         deque<long long int> dq;
+         vector<long long> ans;
+         int negative = -1;
+         
+         //process first window
+         for(int i=0; i<K; i++) { 
+             if(A[i] < 0) {
+                 dq.push_back(i);
+             }
+         }
+         
+         //push ans for FIRST window
+         if(dq.size() > 0) {
+             ans.push_back(A[dq.front()]);
+         }
+         else
+         {
+             ans.push_back(0);
+         }
+         
+         //now process for remaining windows
+         for(int i = K; i<N; i++) {
+             //first pop out of window element
+             
+             
+             if(!dq.empty() && (i - dq.front())>=K ) {
+                 dq.pop_front();
+             }
+             
+             //then push current element
+             if(A[i] < 0)
+                dq.push_back(i);
+             
+            // put in ans
+            if(dq.size() > 0) {
+                 ans.push_back(A[dq.front()]);
+            }
+            else
+            {
+                ans.push_back(0);
+            }
+         }
+         return ans;
+ }
